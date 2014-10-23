dee1ccb447d7ae8785b6b7e707a0b0533422d9db
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-10-23 14:15:19 -0200

    Changed downloader'

--- bin/my_downloader.sh
+++ bin/my_downloader.sh
@@ -8,23 +8,27 @@ OUTPUT_FILE=`echo $BASENAME | cut -d '?' -f 1`
 
 DOWNLOAD_PATH="/home/downloader"
 
+USERNAME=downloader
+
+HOST=rubyjobsbrazil.com.br
+
 ssh_command() {
   CMD=$1
   echo "Executing: $CMD\n"
-  `ssh downloader@rubyjobsbrazil.com.br "echo $CMD >> ~/ssh_commands.log"`
-  `ssh downloader@rubyjobsbrazil.com.br "$CMD"`
+  `ssh $USERNAME@$HOST "echo $CMD >> ~/ssh_commands.log"`
+  `ssh $USERNAME@$HOST "$CMD"`
 }
 
 download_file() {
   FILE=$1
   echo "Dowloading ${FILE}..."
-  CMD="scp downloader@rubyjobsbrazil.com.br:$FILE ."
+  CMD="scp $USERNAME@$HOST:$FILE ."
   `$CMD`
 }
 
 if [ -f $OUTPUT_FILE ]; then
   echo "File ${OUTPUT_FILE} was already downloaded."
 else
-  ssh_command "wget --no-check-certificate -c $URL -O $OUTPUT_FILE" && \
+  ssh_command "cd $DOWNLOAD_PATH && wget --no-check-certificate -c $URL -O $OUTPUT_FILE" && \
   download_file "$DOWNLOAD_PATH/$OUTPUT_FILE"
 fi
