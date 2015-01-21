6409645a3a4ab1964aff2574334c7025330ab040
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-01-21 14:21:13 +0100

    Added instalation for cmake and python-dev

--- bin/ubuntu_tweaks.sh
+++ bin/ubuntu_tweaks.sh
@@ -10,3 +10,11 @@ function mecho() {
 
 mecho "Installing silversearcher-ag, tmux, ctags"
   sudo apt-get install silversearcher-ag tmux ctags
+
+# you complete me requirements
+# https://github.com/Valloric/YouCompleteMe
+mecho "Installing build-essential cmake"
+  sudo apt-get install build-essential cmake
+
+mecho "Installing python-dev"
+  sudo apt-get install python-dev
