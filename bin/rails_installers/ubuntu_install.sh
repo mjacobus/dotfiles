5e758a3451c8e52610302e7a4375db340ec6d889
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-03-04 11:46:04 +0100

    Fixed instalation of rvm

--- bin/rails_installers/ubuntu_install.sh
+++ bin/rails_installers/ubuntu_install.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-echo "Installing mysql-client, libmysqlclient-dev and lots of other libs ..."
+function mecho() {
+  echo -e '\E[32m'"\033[1m"
+  echo "#############################################################################"
+  echo "# $1"
+  echo "#############################################################################"
+  echo -e "\033[0m"
+}
+
+mecho "Installing mysql-client, libmysqlclient-dev and lots of other libs ..."
   sudo apt-get install mysql-client zlib1g-dev libssl-dev libsqlite3-dev libmysqlclient-dev \
     imagemagick librmagick-ruby libxml2-dev libxslt1-dev build-essential openssl \
     libreadline6 libreadline6-dev zlib1g libyaml-dev libsqlite3-0 sqlite3 libxml2-dev \
     libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison libpq-dev libpq5 \
     libmysql-ruby libmysqlclient-dev
 
-echo "Installing curl..."
+mecho "Installing curl..."
   sudo apt-get install curl
 
-echo "Installing RVM (Ruby Version Manager) and Latest Ruby, which becomes the default ..."
+mecho "Installing RVM (Ruby Version Manager) and Latest Ruby, which becomes the default ..."
   curl -L https://get.rvm.io | bash -s stable --ruby
 
-echo "Adding rvm path to the ~/.bashrc file ..."
-  source ~/.bash_profile
-  echo 'source ~/.bash_profile' >> ~/.bashrc
+mecho "Adding rvm path to the ~/.bashrc file ..."
+  source ~/.dotfiles/shell/rvm.sh
 
-echo "Installing critical Ruby gems for Rails development ..."
+mecho "Installing critical Ruby gems for Rails development ..."
   gem install bundler rails pg foreman thin --no-rdoc --no-ri
 
