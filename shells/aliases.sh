1c55d8d439cd51f464afbf973b35723bde3bbdb0
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-08-29 01:10:06 +0200

    Fixed clipboard

--- shells/aliases.sh
+++ shells/aliases.sh
@@ -15,5 +15,8 @@ alias t='phpunit'
 
 # conditional alias
 which vim.gnome > /dev/null 2>&1 && alias vim=vim.gnome
-which mvim > /dev/null 2>&1 && alias vim=mvim
+which mvim > /dev/null 2>&1 && alias vim='mvim -v'
+which mvim > /dev/null 2>&1 && alias gvim='mvim -v'
+which gvim > /dev/null 2>&1 && alias vim='gvim -v'
+which gvim > /dev/null 2>&1 && alias gvim='gvim -v'
 which nvim > /dev/null 2>&1 && alias vim=nvim
