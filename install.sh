9ab4c60bf4a08884181fc050fb5df506f008ca6c
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2016-02-05 10:41:12 +0100

    Fixed nvim config

--- install.sh
+++ install.sh
@@ -56,6 +56,14 @@ symlink_or_ask ~/.dotfiles/ctags ~/.ctags
 symlink_or_ask ~/.dotfiles/agignore ~/.agignore
 symlink_or_ask ~/.dotfiles/composer ~/.composer
 
+# Fixes nvim config
+# https://github.com/neovim/neovim/issues/3499
+# https://github.com/ZyX-I/neovim/blob/42047acb4f07c689936b051864c6b4448b1b6aa1/runtime/doc/nvim_from_vim.txt#L12-L18
+if [[ -d ~/.config ]]; then
+  symlink_or_ask ~/.nvim ~/.config/nvim
+  symlink_or_ask ~/.nvimrc ~/.config/nvim/init.vim
+fi
+
 # install vundle
 if [ ! -f ~/.dotfiles/vim/Vundle.vim ]; then
   cd ~/.dotfiles/vim && git clone https://github.com/gmarik/Vundle.vim.git
