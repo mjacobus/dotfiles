51a5f37983692a50d057c89a8eeec243dcf10b40
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2016-02-17 11:12:34 +0100

    Improved aliases

--- shells/aliases.sh
+++ shells/aliases.sh
@@ -1,17 +1,27 @@
 # Aliases.
 alias l='ls -alF'
 
+alias dotfiles='cd ~/.dotfiles'
+
 # git
 alias gs='git status -s'
 alias gc='git commit'
 alias gp='git push origin'
 alias gpr='git pull --rebase'
+alias gprm='git pull --rebase origin master'
 alias gpu='git pull'
 
-alias dotfiles='cd ~/.dotfiles'
-
-alias ag='/usr/bin/ag'
+# php
+alias phpunit='./vendor/bin/phpunit'
+alias php-cs-fixer='./vendor/bin/php-cs-fixer'
+alias phpmd='./vendor/bin/phpmd'
 alias t='phpunit'
+
+# ruby
+alias carrasco='bundle exec carrasco'
+alias cap='bundle exec cap'
+
+alias ag=$(which ag)
 
 # conditional alias
 which vim.gnome > /dev/null 2>&1 && alias vim=vim.gnome
@@ -20,3 +30,4 @@ which mvim > /dev/null 2>&1 && alias gvim='mvim -v'
 which gvim > /dev/null 2>&1 && alias vim='gvim -v'
 which gvim > /dev/null 2>&1 && alias gvim='gvim -v'
 which nvim > /dev/null 2>&1 && alias vim=nvim
+
