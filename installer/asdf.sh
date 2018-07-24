ad8831288941d2d1e0da5da7eb79973ff16cbff1
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2018-07-24 22:15:25 +0200

    Add configs for asdf and fzf

---
+++ installer/asdf.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+symlink_or_ask ~/.dotfiles/asdfrc ~/.asdfrc
+
+if [ ! -d ~/.asdf ]; then
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.0
+fi
+
+source ~/.profile.d/asdf.conf
+
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
