9b72996079a3ea8dc976ed9ee0be2dd9902206f1
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-07-11 09:09:11 -0300

    Add tools versions

--- installer/asdf.sh
+++ installer/asdf.sh
@@ -3,6 +3,7 @@
 set -e
 
 symlink_or_ask ~/.dotfiles/asdfrc ~/.asdfrc
+symlink_or_ask ~/.dotfiles/.tools-versions ~
 
 if [ ! -d ~/.asdf ]; then
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.0
