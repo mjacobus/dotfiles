6cb6db2c7a8adf5b3d4155eb404b71268d0129d8
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-01-21 10:07:52 -0300

    Fix installer

--- installer/php_utils.sh
+++ installer/php_utils.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-symlink_or_ask ~/.dotfiles/composer ~/.composer
-
 if [ ! -e ~/.dotfiles/bin/composer.phar ]; then
   curl http://getcomposer.org/composer.phar -o ~/.dotfiles/bin/composer.phar
   chmod +x ~/.dotfiles/bin/composer.phar
 fi
+
+symlink_or_ask ~/.dotfiles/composer ~/.composer
+
 
 cd ~/.dotfiles/composer && ~/.dotfiles/bin/./composer.phar install
 cd -
