883db0168a63bc0d9771ca891fc285904eca5ae1
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-02-14 22:08:48 -0300

    Update asdf dependencies

--- installer/asdf.sh
+++ installer/asdf.sh
@@ -13,3 +13,5 @@ source ~/.profile.d/asdf.conf
 
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
