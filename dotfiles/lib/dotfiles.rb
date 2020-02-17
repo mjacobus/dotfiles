899141586d66b85e4b5853224ca5b0936f65c4af
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-02-16 21:41:23 -0300

    Add draft of dotfiles installer in ruby

---
+++ dotfiles/lib/dotfiles.rb
@@ -0,0 +1,6 @@
+require "dotfiles/version"
+
+module Dotfiles
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ dotfiles/spec/dotfiles_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Dotfiles do
+  it "has a version number" do
+    expect(Dotfiles::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
