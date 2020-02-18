903b0a68d0c9ae8f3785e6b81833520165e2c0af
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-02-18 12:42:18 -0300

    Remove spacemacs

--- installer/spacemacs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -d "$HOME/.emacs.d" ]; then
-  mv ~/.emacs.d ~/.emacs.d.bkp
-fi
-
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-
-symlink_or_ask ~/.dotfiles/spacemacs ~/.spacemacs
