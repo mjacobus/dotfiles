b8f4ef6e0bea5464d219c13d78cdca7556d32e4f
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-08-25 17:56:39 +0000

    Do not install neovim with vim

--- lib/dotfiles/installers/vim.rb
+++ lib/dotfiles/installers/vim.rb
@@ -13,13 +13,10 @@ module Dotfiles
       def perform_installation
         symlink(dotfile('vim'), destination_for('.vim'))
         symlink(dotfile('vim/vimrc'), destination_for('.vimrc'))
-        symlink(dotfile('vim/nvimrc'), destination_for('.nvimrc'))
-        symlink(dotfile('vim'), destination_for('.nvim'))
         symlink(dotfile('vim/ideavimrc'), destination_for('.ideavimrc'))
         symlink(dotfile('vim/gvimrc'), destination_for('.gvimrc'))
 
         app.run("mkdir -p #{app.home}/.config")
-        symlink(destination_for('.nvim'), destination_for('.config/nvim'))
         app.run('vim +PlugInstall +quitall')
       end
       # rubocop:enable Metrics/AbcSize
