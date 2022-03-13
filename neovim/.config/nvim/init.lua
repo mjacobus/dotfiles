84529cc261a0267017f34fbdd509f56b9685b1ba
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2022-03-13 20:27:09 -0300

    Neovim: Add mappings for mj alternative_file

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -27,3 +27,37 @@ vimp.nnoremap('<leader>j', function()
     -- vim.api.nvim_exec('<cr>', true)
   end
 end)
+
+vimp.nnoremap('<leader>ak', function()
+  open_mj_alternative_file('next', '--exists')
+end)
+
+vimp.nnoremap('<leader>aj', function()
+  open_mj_alternative_file('prev', '--exists')
+end)
+
+function open_mj_alternative_file(subcommand, options)
+  file_path = vim.fn.expand('%')
+  files = mj_alternative_file(file_path, subcommand, options)
+  print("--files " .. files)
+  files = vim.split(files, ' ')
+  file = files[1]
+
+  print(" file: " .. file)
+  if file ~= '' then
+    vim.api.nvim_command('e ' .. file)
+  end
+end
+
+function mj_alternative_file(file, subcommand, options)
+  local cmd = 'mj alternative_file ' .. subcommand .. ' ' .. file .. ' ' .. options
+  return execute_command(cmd)
+end
+
+function execute_command(cmd)
+  print("cmd: " .. cmd)
+  local f = io.popen(cmd)
+  local s = f:read('*a')
+  f:close()
+  return s
+end
