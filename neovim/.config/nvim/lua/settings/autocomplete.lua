c8e6dd36eed8a6a73e9b098896effb5440a0a9ee
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2022-11-04 10:24:05 -0300

    Suppress warning

--- neovim/.config/nvim/lua/settings/autocomplete.lua
+++ neovim/.config/nvim/lua/settings/autocomplete.lua
@@ -86,7 +86,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 --  require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
 --    capabilities = capabilities
