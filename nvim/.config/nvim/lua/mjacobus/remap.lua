c3873778002f4b6077ea20e6ef157714a83a05b4
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-03-28 11:09:08 -0300

    Disable mappings that conflict with my old mappings

--- nvim/.config/nvim/lua/mjacobus/remap.lua
+++ nvim/.config/nvim/lua/mjacobus/remap.lua
@@ -33,10 +33,10 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
@@ -51,7 +51,7 @@ end)
 vim.keymap.set("i", "jj", "<ESC>:w<cr>")
 vim.keymap.set("n", "<leader>s", ":w<cr>")
 
-vim.keymap.set("n", '<leader>j', function()
+vim.keymap.set("n", '<leader>d', function()
   name = vim.api.nvim_buf_get_name(0)
 
   if string.find(name, "term://") then
