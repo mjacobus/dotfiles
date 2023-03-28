b0cfd49ae3eafadf6f7866c0fdea2ae56335ccbe
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-03-28 16:55:00 -0300

    Better zen mode

--- nvim/.config/nvim/after/plugin/zenmode.lua
+++ nvim/.config/nvim/after/plugin/zenmode.lua
@@ -2,7 +2,7 @@
 vim.keymap.set("n", "<leader>zz", function()
     require("zen-mode").setup {
         window = {
-            width = 90,
+            width = 120,
             options = { }
         },
     }
@@ -10,14 +10,13 @@ vim.keymap.set("n", "<leader>zz", function()
     vim.wo.wrap = false
     vim.wo.number = true
     vim.wo.rnu = true
-    -- ColorMyPencils()
 end)
 
 
 vim.keymap.set("n", "<leader>zZ", function()
     require("zen-mode").setup {
         window = {
-            width = 80,
+            width = 120,
             options = { }
         },
     }
@@ -26,5 +25,4 @@ vim.keymap.set("n", "<leader>zZ", function()
     vim.wo.number = false
     vim.wo.rnu = false
     vim.opt.colorcolumn = "0"
-    -- ColorMyPencils()
 end)
