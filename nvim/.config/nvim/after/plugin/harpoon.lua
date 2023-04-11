cc45c80eed2aac69d31bc4b059c0cc5d25ad8289
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-04-11 09:01:58 -0300

    Nvim: Configure harpoon

--- nvim/.config/nvim/after/plugin/harpoon.lua
+++ nvim/.config/nvim/after/plugin/harpoon.lua
@@ -1,7 +1,9 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.toggle_file)
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader><leader>a", mark.rm_file)
+vim.keymap.set("n", "<leader><leader>dd", mark.clear_all)
 vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
