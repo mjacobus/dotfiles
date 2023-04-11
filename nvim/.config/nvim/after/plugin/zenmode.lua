2517bc25dcd892357ee86d5c9d5eecd533ef0a54
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-04-11 09:02:13 -0300

    Nvim: Configure zenmode

--- nvim/.config/nvim/after/plugin/zenmode.lua
+++ nvim/.config/nvim/after/plugin/zenmode.lua
@@ -9,7 +9,6 @@ vim.keymap.set("n", "<leader>zz", function()
     require("zen-mode").toggle()
     vim.wo.wrap = false
     vim.wo.number = true
-    vim.wo.rnu = true
 end)
 
 
@@ -23,6 +22,5 @@ vim.keymap.set("n", "<leader>zZ", function()
     require("zen-mode").toggle()
     vim.wo.wrap = false
     vim.wo.number = false
-    vim.wo.rnu = false
     vim.opt.colorcolumn = "0"
 end)
