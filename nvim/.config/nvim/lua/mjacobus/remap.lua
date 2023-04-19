676f849fbbc494faf40d5a9c4bcfe56e718c224c
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-04-19 12:12:22 -0300

    Fix join mapping in visual mode

--- nvim/.config/nvim/lua/mjacobus/remap.lua
+++ nvim/.config/nvim/lua/mjacobus/remap.lua
@@ -1,8 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<c-J>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<c-K>", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
