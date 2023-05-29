3ceef0d9d102a3c9eccae83c31e58f034c88e2d2
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-05-29 14:43:14 -0300

    Install copilot

--- lvim/.config/lvim/lua/config/plugins.lua
+++ lvim/.config/lvim/lua/config/plugins.lua
@@ -16,6 +16,18 @@ return {
     dependencies = { { 'mattn/webapi-vim' } }
   },
 
+  {
+    "zbirenbaum/copilot-cmp",
+    event = "InsertEnter",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function()
+      vim.defer_fn(function()
+        require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+        require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+      end, 100)
+    end,
+  },
+
   -- misc
   { 'tpope/vim-surround' },
   { 'tpope/vim-eunuch' },
