776aae496520ded95eb5f86a2d982f2a726c9e99
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-06-07 14:16:34 -0300

    Fix some mappings

--- lvim/.config/lvim/after/plugin/telescope.lua
+++ lvim/.config/lvim/after/plugin/telescope.lua
@@ -1,16 +1,17 @@
 local builtin = require('telescope.builtin')
 
 local git_files = function()
-  builtin.git_files({ show_untracked=true })
+  builtin.git_files({ show_untracked = true })
 end
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
--- vim.keymap.set('n', '<leader>p', git_files, {}) -- conflict
 vim.keymap.set('n', '<leader>pa', git_files, {})
 vim.keymap.set('n', '<leader>pg', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>P', ':Telescope git_files find_command=rg,--ignore,--files<cr>')
+
+lvim.builtin.which_key.mappings.p.s = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Current buffer fuzzy find" }
