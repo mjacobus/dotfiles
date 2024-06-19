b86d529878e7d5369f168db7ff5a58ccc4abe91c
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-06-19 20:31:44 -0300

    Change volume in all output devices

--- i3/.bin/change-volume.sh
+++ i3/.bin/change-volume.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+# Function to set volume for all sinks
+set_volume_for_all_sinks() {
+	local volume_change=$1
+	for sink in $(pactl list short sinks | awk '{print $1}'); do
+		pactl set-sink-volume $sink $volume_change
+	done
+}
+
+# Function to toggle mute for all sinks
+toggle_mute_for_all_sinks() {
+	for sink in $(pactl list short sinks | awk '{print $1}'); do
+		pactl set-sink-mute $sink toggle
+	done
+}
+
 if [ "$1" = "up" ]; then
-  pactl set-sink-volume @DEFAULT_SINK@ +5%
+	set_volume_for_all_sinks "+5%"
 elif [ "$1" = "down" ]; then
-  pactl set-sink-volume @DEFAULT_SINK@ -5%
+	set_volume_for_all_sinks "-5%"
 elif [ "$1" = "mute" ]; then
-  pactl set-sink-mute @DEFAULT_SINK@ toggle
+	toggle_mute_for_all_sinks
 fi
 
-pkill -RTMIN+10 i3blocks
+pkill -RTMIN+10 i3bloc
