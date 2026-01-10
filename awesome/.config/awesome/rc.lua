680e0176905715ce4e86bce045960a0df6c21f1b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2026-01-10 15:15:22 -0300

    Tiling on ubuntu

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -65,11 +65,11 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.floating,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
+    awful.layout.suit.floating,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
     awful.layout.suit.spiral,
